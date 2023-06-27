f941c14164299cf6509662b56774ca6fe3358f5c
Nikin-2204 <truonglnh.kv@gmail.com>
2023-06-27 14:35:53 +0700

    update source using recursion to see green4ever

--- src/main/java/org/truonglnh/mathutil/core/MathUtility.java
+++ src/main/java/org/truonglnh/mathutil/core/MathUtility.java
@@ -21,6 +21,20 @@ public class MathUtility {
     // vi giai thua tang rat nha, 21! long k chua noi (long 18 so 0)
     //20! vua du cho long
     //21! 22! 23! > 20! -> chui, nem ra Exception
+//    public static long getFactorial(int n){
+//        
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid number n must be between 0..20, plz");
+//        }
+//        if(n == 0 || n == 1)
+//            return 1;
+//        
+//        long product = 1; /// bien luu don cacs tinh 
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;       
+//        return product;      
+//    }
+    
     public static long getFactorial(int n){
         
         if (n < 0 || n > 20) {
@@ -28,11 +42,17 @@ public class MathUtility {
         }
         if(n == 0 || n == 1)
             return 1;
-        
-        long product = 1; /// bien luu don cacs tinh 
-        for (int i = 2; i <= n; i++) 
-            product *= i;       
-        return product;      
+        return n * getFactorial(n - 1);
+              
+             
     }
 }
 
+//5! = 1.2.3.4.5 = 1.2.3.4   .5
+//               = 4!        .5 = 5 x 4!
+
+//4! = 4 x 3!
+//3! = 3 x 2!
+//2! = 2 x 1!
+//1! = 1; dung quá trình duoi nhau
+//N! = N x (N - 1)!
--- src/test/java/org/truonglnh/mathutil/core/test/MathUtilityAdvancedTest.java
+++ src/test/java/org/truonglnh/mathutil/core/test/MathUtilityAdvancedTest.java
@@ -21,7 +21,7 @@ public class MathUtilityAdvancedTest {
                                {1, 1},
                                {2, 2},
                                {4, 24},
-                               {6, 72000}};
+                               {6, 720}};
         return testData;
     }
     
